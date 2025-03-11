e3836d2198a4271f2ee60c2908b72362f83a6bbb
Isac Silva <141184998+Isac-Silva@users.noreply.github.com>
2025-03-11 09:23:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,61 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        dots: true,
+        arrows: false
+    });
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
-
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
-
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
+    
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
+    
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    });
+    
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            }
         },
-        email: {
-            requerido: true,
-            email: true
+        messages: {
+            nome: "Por favor, insira seu nome completo.",
+            email: "Insira um e-mail válido.",
+            telefone: "Informe um telefone válido.",
+            endereco: "Preencha o endereço completo.",
+            cep: "Digite um CEP válido.",
+            cpf: "Informe um CPF válido."
         },
-        telefone: {
-            requerido: true
+        submitHandler: function(form) {
+            alert('Sua requisição foi enviada para análise, parabéns pela aquisição!');
+            form.reset();
         },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+        invalidHandler: function(event, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos. Por favor, corrija-os e tente novamente.`);
+            }
+        }
+    });
+});
